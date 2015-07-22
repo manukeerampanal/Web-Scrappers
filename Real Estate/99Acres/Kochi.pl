3e0b54103e931272a0d43a11235cf8dbbd2794ff
Shibu P U <shibu.pu@gmail.com>
2015-07-22 16:14:29 +0530

    Class name changed in 99acres.com

--- Real Estate/99Acres/Kochi.pl
+++ Real Estate/99Acres/Kochi.pl
@@ -22,10 +22,11 @@ my $tree = HTML::TreeBuilder->new_from_content(decode_utf8($mech->content()));
 
 my @divs      = $tree->look_down(_tag => 'div', class => 'wrapttl');
 my @desc_divs = $tree->look_down(_tag => 'div', class => 'lf  f12');
-my @time_divs = $tree->look_down(_tag => 'div', class => 'rf f11 mt25');
-#print scalar @divs . "\n";
-#print scalar @desc_divs . "\n";
-#print scalar @time_divs . "\n";
+# my @time_divs = $tree->look_down(_tag => 'div', class => 'rf f11 mt25');
+my @time_divs = $tree->look_down(_tag => 'div', class => 'lf f13 hm10 mb5');
+print scalar @divs . "\n";
+print scalar @desc_divs . "\n";
+print scalar @time_divs . "\n";
 #exit;
 
 my $count = 0;
@@ -37,7 +38,7 @@ for my $div (@divs) {
     $summary    =~ s/^Description : //;
     my $link    = $base_url . $div->look_down(_tag => 'a')->attr('href');
     my $time    = $time_divs[$count]->as_trimmed_text;
-    $time       =~ s/^Posted : //;
+    $time       =~ s/^.+?Posted : //;
     #my @a       = $div->look_down(_tag => 'a', class => 'defultchi2');
     #my $type    = $a[0]->as_trimmed_text;
 
@@ -48,7 +49,7 @@ for my $div (@divs) {
     print "price: $price\n";
     print "time: $time\n";
     print "link: $link\n";
-    exit;
+    # exit;
 
     $count++;
     print "count: $count\n\n";
